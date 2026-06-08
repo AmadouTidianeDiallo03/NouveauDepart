dfd96913e2a52249d3d6bf0a9bb8aec06d9117b0
Amadou Tidiane Diallo <tidianeFlow@outlook.com>
2026-06-07 23:20:56 -0400

    Align frontend API prefix with Railway backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import API_URL from "../config/api";
 
-const API_BASE_URL = `${API_URL.replace(/\/$/, "")}/api`;
+const API_BASE_URL = `${API_URL.replace(/\/$/, "")}/API`;
 
 const api = axios.create({
     baseURL: API_BASE_URL,
